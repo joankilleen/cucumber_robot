cb690a6ef69700fb24666a30c0cd952343d12312
Joan Killeen <joan.killeen@bluewin.ch>
2018-10-14 16:25:06 +0200

    improved project structure

--- src/test/java/runner/RobotRunnerTest.java
+++ src/test/java/runner/RobotRunnerTest.java
@@ -6,7 +6,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features={"src/test/java/features/"},
+@CucumberOptions(features={"src/test/resources/features/"},
 glue={"step_definitions"}, 
 monochrome = true,
 plugin = { "pretty" })
